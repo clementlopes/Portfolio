27429494ab081d313fc46e2b8c455184d107e1ea
Clément Lopes <clementlopes02@gmail.com>
2025-12-09 21:09:51 +0000

    ✨ feat(app): Add SEO metadata and favicon links, remove duplicate head 🛠️ config(nuxt): Add favicon links to app head configuration 🗑️ fix(index): Remove redundant useHead from index.vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,4 +11,14 @@ export default defineNuxtConfig({
     modules: [
         '@pinia/nuxt',
     ],
+    app: {
+        head: {
+            link: [
+
+                { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+                { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+                { rel: 'shortcut icon', href: '/favicon.ico' }
+            ]
+        }
+    }
 });
